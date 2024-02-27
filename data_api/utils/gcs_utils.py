91e4f09e85c839ca00c94a629a170ff78d14bb11
Vivardhan Kanoria <vivardhan@gmail.com>
2024-02-27 00:52:56 -0800

    Add new GCS util function

--- data_api/utils/gcs_utils.py
+++ data_api/utils/gcs_utils.py
@@ -1,3 +1,6 @@
+# System Imports
+from typing import List
+
 # Third Party Imports
 from google.cloud import storage
 
@@ -10,3 +13,12 @@ def file_exists_gcs(client_provider: GoogleClientProvider, filepath: str) -> boo
 def upload_to_gcs(client_provider: GoogleClientProvider, filepath: str) -> None:
 	print("Uploading to GCS: {}".format(filepath))
 	client_provider.DATA_BUCKET.blob(filepath).upload_from_filename(filepath)
+
+def list_files_gcs(client_provider: GoogleClientProvider, prefix: str, extension: str) -> List:
+	return [
+		f.name for f in client_provider.DATA_BUCKET.list_blobs(prefix=prefix)
+		if f.name.endswith(extension)
+	]
+
+def download_file_gcs(client_provider: GoogleClientProvider, filepath: str) -> None:
+	client_provider.DATA_BUCKET.blob(filepath).download_to_filename(filepath)
