687fde786590949b6fd387efb867c1f5a8bfd695
Vivardhan Kanoria <vivardhan@gmail.com>
2024-02-28 16:00:49 -0800

    New abstract class for QA generation using LLMs

---
+++ data_api/qa_generator/llm_qa_generator.py
@@ -0,0 +1,95 @@
+# System Imports
+import abc
+from typing import List, Tuple
+
+class LLMQAGenerator(metaclass=abc.ABCMeta)::
+	"""
+	This is an abstract class which specifies an interface for 
+	using LLMs to convert podcast transcripts into question answer pairs.
+	"""
+
+	def __init__(self, model_name: str, seed: int):
+		self.model = load_llm(model_name, seed)
+		# Perform any additional setup in your derived class, 
+		# eg. inference settings can be initialized here
+
+	@abc.abstractmethod
+	def load_llm(self, model_name: str, seed: int) -> None:
+		"""
+		Loads the LLM and assigns it to self.model
+
+		params:
+			model_name: The string identifier for the model
+			seed: The seed to initialize the model with
+		
+		returns:
+			None
+		"""
+		pass
+
+	@abc.abstractmethod
+	def create_transcript_prompt(self, transcript: str) -> str:
+		"""
+		Given a transcript file, creats a prompt to run inference with.
+
+		params:
+			transcript: The string from the transcript file
+
+		returns:
+			The prompt text
+		"""
+		pass
+
+	@abc.abstractmethod
+	def run_model_inference(self, prompt: str) -> str:
+		"""
+		Runs inference on self.model using the provided prompt.
+
+		params:
+			prompt: The prompt text
+
+		returns:
+			The model output text
+		"""
+		pass
+
+	@abc.abstractmethod
+	def parse_model_output(self, model_output: str) -> List[Tuple[str, str]]:
+		"""
+		Parse the model output into a list of question answer pairs
+
+		params:
+			model_output string
+
+		returns
+			a List of question answer tuples, i.e.:
+			[
+				(question_1, answer_1),
+				...
+				(question_n), answer_n),
+			]
+		"""
+		pass
+
+	def generate_question_answer_pairs(self, transcript: str) -> List[Tuple[str, str]]:
+		"""
+		Generates QA pairs using self.model for a transcript	
+
+		You do not need to re-implement this function for a child class
+
+		params:
+			transcript: The text for a transcript
+
+		returns:
+			a List of question answer tuples, i.e.:
+			[
+				(question_1, answer_1),
+				...
+				(question_n), answer_n),
+			]
+		"""
+		prompt = self.create_transcript_prompt(transcript)
+		output = self.run_model_inference(prompt)
+		return self.parse_model_output(output)
+
+
