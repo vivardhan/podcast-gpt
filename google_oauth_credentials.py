ffda417857b0e82daeb1d7746a780735c613ec8f
Vivardhan Kanoria <vivardhan@gmail.com>
2024-03-02 15:43:46 -0800

    Use a different method to get user credentials

--- google_oauth_credentials.py
+++ google_oauth_credentials.py
@@ -1,11 +1,13 @@
 # System imports
+import json
 import os
 from typing import List
 
 # Third Party Imports
-import google_auth_oauthlib.flow
+import google_auth_oauthlib
+import google.oauth2.credentials
 
-def obtain_google_oauth_credentials(scopes: List[str]):
+def obtain_google_oauth_credentials(scopes: List[str]) -> google.oauth2.credentials.Credentials:
     """
     Obtain google oauth2 credentials for the provided scopes
 
@@ -21,8 +23,20 @@ def obtain_google_oauth_credentials(scopes: List[str]):
 
     # Retrieve oauth credetials from the stored file
     client_secrets_file = os.path.join("assets", "google_oauth_token.json")
+    with open(client_secrets_file, "r") as f:
+        js = json.load(f)
+        client_id = js["web"]["client_id"]
+        client_secret = js["web"]["client_secret"]
+
+        f.close()
 
     # Get credentials
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    return flow.run_local_server()
+    # flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+    #     client_secrets_file, scopes)
+    # return flow.run_local_server()
+
+    return google_auth_oauthlib.get_user_credentials(
+        scopes=scopes, 
+        client_id=client_id, 
+        client_secret=client_secret,
+    )
