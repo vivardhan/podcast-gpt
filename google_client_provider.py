e7d97da534c18fb5c96215b8558649cf0fb254f8
Vivardhan Kanoria <vivardhan@gmail.com>
2024-03-03 10:38:15 -0800

    Clean up code and add comments

--- google_client_provider.py
+++ google_client_provider.py
@@ -6,14 +6,23 @@ from google.cloud import storage
 # Package imports
 from google_oauth_credentials import obtain_google_oauth_credentials
 
-# Set the scope to be youtube readonly
+# Set the scopes for authentication
 SCOPES = [
+    # For google cloud storage read write on the podcast-gpt project
     "https://www.googleapis.com/auth/devstorage.read_write",
+    # For reading youtube data using the Youtube Data API v3
     "https://www.googleapis.com/auth/youtube.readonly",
 ]
 PROJECT_ID = "podcast-gpt-415000"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
 
 class GoogleClientProvider:
+    """
+     This is an instance class so that a given binary only has one copy
+     Clients are expected to instantiate it once in their main() method
+     and pass the instance to objects that need to access google APIs.
+    """
     _instance = None
 
     def __new__(cls):
@@ -26,10 +35,7 @@ class GoogleClientProvider:
             cls.DATA_BUCKET = cls.STORAGE_CLIENT.get_bucket("podcast_gpt_data")
 
             # Create the youtube data API client
-            youtube_api_service_name = "youtube"
-            youtube_api_version = "v3"
-            
             cls.youtube_client = googleapiclient.discovery.build(
-                youtube_api_service_name, youtube_api_version, credentials=cls.CREDENTIALS)
+                YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, credentials=cls.CREDENTIALS)
 
         return cls._instance
