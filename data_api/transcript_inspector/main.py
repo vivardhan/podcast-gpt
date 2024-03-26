bcbda73cd9c9a654040bdb568dd1332b531feea2
Vivardhan Kanoria <vivardhan@gmail.com>
2024-03-25 18:19:35 -0700

    Add new stats to transcript inspector

--- data_api/transcript_inspector/main.py
+++ data_api/transcript_inspector/main.py
@@ -1,4 +1,5 @@
 # System Imports
+import json
 import os
 
 # Package Imports
@@ -24,9 +25,25 @@ def main():
 			total_words += len(text.split())
 			file_count += 1
 			max_file_size = max(max_file_size, len(text.split()))
+
+		chapterized_transcript_folder = Paths.get_chapterized_data_folder(podcast.name)
+		chapterized_files = list_files_gcs(gc_provider, chapterized_transcript_folder, Paths.JSON_EXT)
+
+		chapters_count = 0
+		max_chapter_size = 0
+		for cf in chapterized_files:
+			chapters = json.loads(download_textfile_as_string_gcs(gc_provider, cf))
+			for chapter_title, chapter_content in chapters.items():
+				chapters_count += 1
+				num_words = len(chapter_content.split())
+				max_chapter_size = max(max_chapter_size, num_words)
+
+
 		print("{} has {} words".format(podcast.name, total_words))
 		print("{} has a maximum of {} words in one episode".format(podcast.name, max_file_size))
 		print("{} has {} words on an average in each episode".format(podcast.name, total_words/file_count))
+		print("{} has a maximum of {} words in one chapter".format(podcast.name, max_chapter_size))
+		print("{} has {} words on an average in each chapter".format(podcast.name, total_words/chapters_count))
 
 if __name__ == "__main__":
 	main()
