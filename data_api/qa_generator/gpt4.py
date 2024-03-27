a185e0a9f78fc31b0fd1ec62fbfde888a4b5b21a
Vivardhan Kanoria <vivardhan@gmail.com>
2024-03-27 01:19:49 -0700

    Misc. changes to QA generation code

--- data_api/qa_generator/gpt4.py
+++ data_api/qa_generator/gpt4.py
@@ -48,22 +48,27 @@ class GPT4QAGenerator(LLMQAGenerator):
 		return  """
         The following is an excerpt from a podcast hosted by {}. 
         The title of the current episode of the podcast is {}.
-        The topic being discussed in the current excerpt is {}.
+        The topic being discussed in the current excerpt from this episode is {}.
         
-        Extract a list of question answer pairs from the excerpt, covering the following areas, if possible:
+        Extract question answer pairs from the excerpt, covering the following areas, if possible:
         1. Facts in the discussion.
         2. Advice for resolving an issue.
         3. Strategies for optimizing for specific personal goals.
         4. Explanations of nuanced or complex ideas and topics.
 
-        When framing questions, follow these rules as far as possible:
+        When framing the questions, follow these rules as far as possible:
         1. Ensure that the question makes sense to a reader who is unfamiliar with the content of the excerpt.
-        1. Avoid references to the podcast, speakers, episode title or chapter title.
-        2. Do not assume any knowledge of the provided excerpt by the reader of the question.
+        2. Avoid references to the podcast, speakers, episode title or chapter title.
+        3. Do not assume any knowledge of the provided excerpt by the reader of the question. 
+        4. Ask questions from the point of view of a person who has never read the excerpt. In particular do not use 
+        using phrases such "from the discussion", "was discussed", "in the excerpt", "as described", etc.
 
-        If the questions and answers refer to the podcast host or other speakers, they should do so in the third person.
+
+        If a question or answer refers to the podcast host or other speakers, it should do so in the third person. 
+        Provide detailed, well explained answers and thorough answers. 
+        Except for fact based questions, answers should typically be much longer than the questions.
         """.format(podcast_host, episode_title, chapter_title) + \
-        'Return the question-answer pairs in json format: [\{question: "First question here.", answer:"First answer here."\}]' + \
+        'Return the question-answer pairs in json format: [\{question: "Question here.", answer:"answer here."\}]' + \
         "Excerpt: {}\nQuestion Answer Pairs:\n".format(chapter_transcript)
 
 	def run_model_inference(self, prompt: str) -> str:
--- data_api/qa_generator/main.py
+++ data_api/qa_generator/main.py
@@ -2,6 +2,12 @@
 import json
 import os
 
+# Third Party Imports
+from matplotlib import pyplot as plt
+import numpy as np
+import tiktoken
+from tqdm import tqdm
+
 # Package Imports
 from data_api.utils.gcs_utils import (
 	download_textfile_as_string_gcs,
@@ -21,28 +27,81 @@ qa_generator_models = {
 	"gpt4": GPT4QAGenerator,
 }
 
+def num_tokens_from_string(string: str, encoding_name: str) -> int:
+    """Returns the number of tokens in a text string."""
+    encoding = tiktoken.get_encoding(encoding_name)
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
+
 def main():
 	gc_provider = GoogleClientProvider()
-
 
 	# Uncomment the following once qa_generator_models above is setup
 	for model_name, model_class in qa_generator_models.items():
 		model = model_class(model_name)
+		all_token_counts = []
+		all_large_counts = {}
 		for podcast in PODCASTS:
 			chapterized_data_folder = Paths.get_chapterized_data_folder(podcast.name)
 			qa_pairs_folder = Paths.get_qa_pairs_folder(podcast.name)
 			chapterized_files = list_files_gcs(gc_provider, chapterized_data_folder, Paths.JSON_EXT)
-			for t in chapterized_files:
+			
+			print("Counting tokens for {}".format(podcast.name))
+			for t in tqdm(chapterized_files):
 				text = download_textfile_as_string_gcs(gc_provider, t)
 				chapters_json = json.loads(text)
-				episode_title = os.path.basename(t)[:-len(Paths.JSON_EXT)]
-				chapters_qa_pairs = {
-					chapter: model.generate_question_answer_pairs(podcast.host_name, episode_title, chapter, transcript)
-					for chapter, transcript in chapters_json.items()
+				episode_title = Paths.get_title_from_path(t)
+
+				curr_counts = {
+					chapter_title: num_tokens_from_string(chapter_transcript, "cl100k_base")
+					for chapter_title, chapter_transcript in chapters_json.items()
 				}
 
-				print(chapters_qa_pairs)
-				break
+				large_counts = {
+					chapter_title: count
+					for chapter_title, count in curr_counts.items()
+					if count > 8191
+				}
+
+				if len(large_counts) > 0:
+					all_large_counts[episode_title] = large_counts
+
+				all_token_counts.extend(curr_counts.values())
+
+
+
+
+
+	counts, bins = np.histogram(all_token_counts)
+	plt.figure()
+	plt.hist(bins[:-1], bins, weights=counts)
+	plt.xlabel("Num Tokens")
+	plt.ylabel("Num chapters")
+	plt.savefig('/Users/vkanoria/Desktop/counts.png')
+
+	all_large = []
+	for k, v in all_large_counts.items():
+		print("Episode: {}: \nLong Chapters:".format(k))
+		all_large.extend(v.values())
+		for chapter_name, count in v.items():
+			print("{}: {}".format(chapter_name, count))
+
+	print("Num large files: {}".format(len(all_large)))
+	plt.figure()
+	counts, bins = np.histogram(all_large)
+	plt.hist(bins[:-1], bins, weights=counts)
+	plt.xlabel("Num Tokens")
+	plt.ylabel("Num chapters")
+	plt.savefig('/Users/vkanoria/Desktop/counts_large.png')
+
+				# print("Generating QA pairs for {}".format(episode_title))
+				# chapters_qa_pairs = {
+				# 	chapter: model.generate_question_answer_pairs(podcast.host_name, episode_title, chapter, transcript)
+				# 	for chapter, transcript in chapters_json.items()
+				# }
+
+				# print(chapters_qa_pairs)
+				# break
 					
 				# output_file_path = os.path.join(qa_pairs_prefix, model_name, t)
 				# upload_string_as_textfile_gcs(output_file_path, json.dumps(qa_pairs))
