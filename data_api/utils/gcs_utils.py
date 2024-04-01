3deec9b6ebb2399aca3b398adf120b08719f52d8
Vivardhan Kanoria <vivardhan@gmail.com>
2024-04-01 12:00:24 -0700

    Improve implementation of GCS text download function

--- data_api/utils/gcs_utils.py
+++ data_api/utils/gcs_utils.py
@@ -14,7 +14,7 @@ def upload_to_gcs(client_provider: GoogleClientProvider, filepath: str) -> None:
 	print("Uploading to GCS: {}".format(filepath))
 	client_provider.DATA_BUCKET.blob(filepath).upload_from_filename(filepath)
 
-def list_files_gcs(client_provider: GoogleClientProvider, prefix: str, extension: str) -> List:
+def list_files_gcs(client_provider: GoogleClientProvider, prefix: str, extension: str) -> List[str]:
 	return [
 		f.name for f in client_provider.DATA_BUCKET.list_blobs(prefix=prefix)
 		if f.name.endswith(extension)
@@ -25,12 +25,7 @@ def download_file_gcs(client_provider: GoogleClientProvider, filepath: str) -> N
 	client_provider.DATA_BUCKET.blob(filepath).download_to_filename(filepath)
 
 def download_textfile_as_string_gcs(client_provider: GoogleClientProvider, filepath: str) -> str:
-	tmp_path = "tmp"
-	client_provider.DATA_BUCKET.blob(filepath).download_to_filename(tmp_path)
-	with open(tmp_path, "r") as f:
-		ret = f.read()
-		f.close()
-		return ret
+	return client_provider.DATA_BUCKET.blob(filepath).download_as_text()
 
 def upload_string_as_textfile_gcs(client_provider: GoogleClientProvider, filepath: str, string: str) -> None:
 	print("Uploading to GCS: {}".format(filepath))
