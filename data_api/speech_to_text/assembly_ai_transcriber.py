ad98400cf3ff448c0c5b06f4228a06b7e1104def
Vivardhan Kanoria <vivardhan@gmail.com>
2024-04-22 15:46:40 -0700

    Refactor transcription using Paths instead of custom dataclass

--- data_api/speech_to_text/assembly_ai_transcriber.py
+++ data_api/speech_to_text/assembly_ai_transcriber.py
@@ -1,8 +1,6 @@
 # System Imports
 from concurrent.futures import ProcessPoolExecutor
-from dataclasses import dataclass
 import json
-import os
 import requests
 from typing import List
 
@@ -10,7 +8,10 @@ from typing import List
 import assemblyai as aai
 
 # Package Imports
-from data_api.utils.file_utils import create_temp_local_directory, delete_temp_local_directory
+from data_api.utils.file_utils import (
+    create_temp_local_directory,
+    delete_temp_local_directory,
+)
 from data_api.utils.gcs_utils import GCSClient
 from data_api.utils.paths import Paths
 
@@ -29,42 +30,28 @@ def get_aai_transcript(aai_transcript_id: str) -> str:
     except requests.exceptions.RequestException as e:
         raise SystemExit(e)
 
-@dataclass
-class AudioToTranscribe:
-    """Encapsulates input and output paths for the audio to transcribe"""
-
-    # Audio file input
-    audio_path: str
-
-    # Raw transcript output
-    raw_transcript_path: str
-
-    # Speaker transcript output
-    speaker_transcript_path: str
-
-    # Assembly AI json output
-    aai_transcript_path: str
-
-
 class AudioTranscriber:
 
     def __init__(self, podcast_name: str, extension: str):
         self.podcast_name = podcast_name
         self.extension = extension
 
-    def find_untranscribed_audio_files(self) -> List[AudioToTranscribe]:
+    def find_untranscribed_episodes(self) -> List[str]:
         """
-        Looks for audio files that have not been transcribed for this podcast
+        Looks for episodes that have not been transcribed for this podcast
 
         returns:
-            A list of audio files to transcribe 
+            A list of episode titles to transcribe 
         """
 
         print("Looking for untranscribed_files for {} ...".format(self.podcast_name))
         untranscribed_files = []
 
+        # Get a list of all audio files for this podcast
         audio_folder = Paths.get_audio_data_folder(self.podcast_name)
         audio_files = GCSClient.list_files(audio_folder, self.extension)
+
+        # Create a set of the existing transcripts (raw, speaker labeled and original from Assembly AI)
         raw_transcript_folder = Paths.get_raw_transcript_folder(self.podcast_name)
         raw_transcript_files = set(GCSClient.list_files(raw_transcript_folder, Paths.TXT_EXT))
         speaker_transcript_folder = Paths.get_speaker_transcript_folder(self.podcast_name)
@@ -72,53 +59,58 @@ class AudioTranscriber:
         aai_transcript_folder = Paths.get_aai_transcript_folder(self.podcast_name)
         aai_transcript_files = set(GCSClient.list_files(aai_transcript_folder, Paths.JSON_EXT))
         
-        ext_len = len(self.extension)
         for audio_file in audio_files:
             title = Paths.get_title_from_path(audio_file)
             raw_transcript_file = Paths.get_raw_transcript_path(self.podcast_name, title)
             speaker_transcript_file = Paths.get_speaker_transcript_path(self.podcast_name, title)
             aai_transcript_file = Paths.get_aai_transcript_path(self.podcast_name, title)
 
+            # Check if any of the transcripts do not exist
             if (
                 raw_transcript_file not in raw_transcript_files or 
                 speaker_transcript_file not in speaker_transcript_files or
                 aai_transcript_file not in aai_transcript_files
             ):
-                untranscribed_files.append(
-                    AudioToTranscribe(
-                        audio_path=audio_file,
-                        raw_transcript_path=raw_transcript_file,
-                        speaker_transcript_path=speaker_transcript_file,
-                        aai_transcript_path=aai_transcript_file,
-                    )
-                )
+                untranscribed_files.append(Paths.get_title_from_path(audio_file))
 
         return untranscribed_files
 
-    def transcribe_audio(self, audio_to_transcribe: AudioToTranscribe) -> None:
-        print("Transcribing {}".format(os.path.basename(audio_to_transcribe.audio_path)))
+    def transcribe_audio(self, episode_title: str) -> None:
+        """
+        Use Assembly AI to transcribe the audio and upload the transcripts to GCS
+
+        params:
+            episode_title:
+                The episode title
+
+        returns:
+            None
+        """
+        print("Transcribing {}".format(episode_title))
 
         # Download the audio
-        GCSClient.download_file(audio_to_transcribe.audio_path)
+        audio_path = Paths.get_audio_path(self.podcast_name, episode_title, self.extension)
+        GCSClient.download_file(audio_path)
 
         # Transcribe with AAI
-        transcript = transcriber.transcribe(
-            audio_to_transcribe.audio_path,
-            config=config,
-        )
+        transcript = transcriber.transcribe(audio_path, config=config)
+
+        raw_transcript_path = Paths.get_raw_transcript_path(self.podcast_name, episode_title)
 
         # Save raw transcript locally
-        with open(audio_to_transcribe.raw_transcript_path, "w") as f:
+        with open(raw_transcript_path, "w") as f:
             f.writelines([
                 utterance.text + " \n" for utterance in transcript.utterances
             ])
             f.close()
 
         # Upload raw transcript to GCS
-        GCSClient.upload_file(audio_to_transcribe.raw_transcript_path)
+        GCSClient.upload_file(raw_transcript_path)
+
+        speaker_transcript_path = Paths.get_speaker_transcript_path(self.podcast_name, episode_title)
 
         # Save speaker transcript locally
-        with open(audio_to_transcribe.speaker_transcript_path, "w") as f:
+        with open(speaker_transcript_path, "w") as f:
             for utterance in transcript.utterances:
                 f.write("Speaker {}: \n".format(utterance.speaker))
                 f.write(utterance.text)
@@ -127,11 +119,11 @@ class AudioTranscriber:
             f.close()
 
         # Upload speaker transcript to GCS
-        GCSClient.upload_file(audio_to_transcribe.speaker_transcript_path)
+        GCSClient.upload_file(speaker_transcript_path)
 
         # Upload AAI json transcript to GCS
         GCSClient.upload_string_as_textfile(
-            audio_to_transcribe.aai_transcript_path, 
+            Paths.get_aai_transcript_path(self.podcast_name, episode_title), 
             json.dumps(get_aai_transcript(transcript.id)),
         )
 
@@ -153,7 +145,7 @@ class AudioTranscriber:
             create_temp_local_directory(tf)
 
         # Find untranscribed files among the titles
-        untranscribed_files = self.find_untranscribed_audio_files()
+        untranscribed_files = self.find_untranscribed_episodes()
 
         if len(untranscribed_files) > 0:
             print("Transcribing {} files for {}".format(len(untranscribed_files), self.podcast_name))
