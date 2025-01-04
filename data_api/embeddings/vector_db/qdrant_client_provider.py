ba7d573985185aeb53fff4edbb1a9c53a9d4a593
Vivardhan Kanoria <vivardhan@gmail.com>
2025-01-03 18:07:06 -0800

    Update qdrant URL

--- data_api/embeddings/vector_db/qdrant_client_provider.py
+++ data_api/embeddings/vector_db/qdrant_client_provider.py
@@ -8,6 +8,6 @@ from qdrant_client import QdrantClient
 class QdrantClientProvider:
 
     client = QdrantClient(
-        url="https://6b57b75d-0e64-4089-92a9-942a637167e6.us-east4-0.gcp.cloud.qdrant.io:6333",
+        url="https://23f48a24-53f6-49ad-8a8a-40cc12a68baf.europe-west3-0.gcp.cloud.qdrant.io:6333",
         api_key=os.environ["QDRANT_API_KEY"],
     )
